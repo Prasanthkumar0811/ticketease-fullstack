86115bd205f8aa5941436edae776edd22ab358fd
AzureAD\PrasanthKumar <prasanthkumar@knstek.com>
2026-02-15 12:48:58 +0530

    Index.js has been updated in backend

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/authverify',router)
-app.listen(process.env.PORT,()=>{
+const PORT=process.env.PORT || 3000
+app.listen(PORT,"0.0.0.0",()=>{
     console.log('Started')
 })
